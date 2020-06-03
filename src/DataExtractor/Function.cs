87189bd0f50d44ad8826c33b1aacfa3ce86180b0
Tai Kolker <tkolker@tkolker-ltm1.internal.salesforce.com>
2020-06-03 20:29:09 +0300

    clean up2

--- src/DataExtractor/Function.cs
+++ src/DataExtractor/Function.cs
@@ -20,19 +20,6 @@ namespace DataExtractor
 
     public class Function
     {
-
-        private static readonly HttpClient client = new HttpClient();
-
-        private static async Task<string> GetCallingIP()
-        {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
-
-            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
-
-            return msg.Replace("\n","");
-        }
-
         public async Task FunctionHandler()
         {
             string requestXML = "<AppointmentBookingRequest><RefreshToken>5Aep861i3pidIObecHklRnSH1FnIZsznQb_i3Jo9UC6ey5emPA8bFpnVVfFu5kexGfE0sWAb1qtfPkJLVQsT4Sd</RefreshToken><IsEmergency>false</IsEmergency><ServiceID>08pe0000000CbHtAAK</ServiceID><IsTest>true</IsTest><IsManaged>false</IsManaged><SchedulingPolicyID>a0Qe000000AzjJNEAZ</SchedulingPolicyID><InstanceName>CS15</InstanceName><OrganizationId>00De0000005T9GFEA0</OrganizationId><OrganizationType>Enterprise Edition</OrganizationType><TravelUnit>km</TravelUnit><SearchSlotsMaxDays>10</SearchSlotsMaxDays><ApprovedAbsences>true</ApprovedAbsences></AppointmentBookingRequest>";
@@ -50,6 +37,7 @@ namespace DataExtractor
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+                LambdaLogger.Log(ex.Message + "\n" + ex.StackTrace);
             }
         }
     }
